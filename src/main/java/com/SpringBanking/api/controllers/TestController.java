0c497c004244a4bd41746f421f55400f1955c39c
Seujumon <jseura@gmail.com>
2023-11-14 19:19:59 -0300

    Creation of the project with Spring Initializr and making the first configurations.

---
+++ src/main/java/com/SpringBanking/api/controllers/TestController.java
@@ -0,0 +1,13 @@
+package com.SpringBanking.api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping
+    public String test(){
+        return "Hello from Spring Banking";
+    }
+}
