0614f1baf00f7d6cd1eab6073638717c825a63c0
VillalbaLucas <lukasvillalba2016@gmail.com>
2023-11-22 10:13:10 -0300

    add: deletedById in UserController

--- src/main/java/com/SpringBanking/api/controllers/UserController.java
+++ src/main/java/com/SpringBanking/api/controllers/UserController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,6 +54,15 @@ public class UserController {
                     .status(HttpStatus.NOT_FOUND)
                     .body(e.toString());
         }
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<?> deleteUserById(@PathVariable Long id){
+
+        if(userService.deleteById(id)){
+            return ResponseEntity.ok().body(null);
+        } else
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se pudo borrar el Usuario");
     }
 
 }
