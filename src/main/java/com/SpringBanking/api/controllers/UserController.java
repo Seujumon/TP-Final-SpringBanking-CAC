4a2859fa9aac1a7f30d1dfd608db1f13d5c4da57
VillalbaLucas <lukasvillalba2016@gmail.com>
2023-11-22 09:49:00 -0300

    add: getUserById in UserController

--- src/main/java/com/SpringBanking/api/controllers/UserController.java
+++ src/main/java/com/SpringBanking/api/controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.SpringBanking.api.controllers;
 
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.SpringBanking.api.models.User;
 import com.SpringBanking.api.models.dto.UserDto;
 import com.SpringBanking.api.services.UserService;
 
@@ -17,28 +21,38 @@ public class UserController {
 
     private final UserService userService;
 
-    public UserController(UserService _userService){
-        userService=_userService;
+    public UserController(UserService _userService) {
+        userService = _userService;
     }
-    
+
     @GetMapping("/users")
-    public ResponseEntity<?> getUsers(){
+    public ResponseEntity<?> getUsers() {
         return ResponseEntity
-            .status(HttpStatus.OK)
-            .body(userService.findAll());
+                .status(HttpStatus.OK)
+                .body(userService.findAll());
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable Long id) {
+        Optional<User> user = userService.findById(id);
+
+        if (user.isPresent())
+            return ResponseEntity.ok().body(user.get());
+        else
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(user.get());
     }
 
     @PostMapping("/users")
-    public ResponseEntity<?> saveUser(@RequestBody UserDto userDto){
+    public ResponseEntity<?> saveUser(@RequestBody UserDto userDto) {
         try {
             return ResponseEntity
-                .ok()
-                .body(userService.save(userDto));
+                    .ok()
+                    .body(userService.save(userDto));
         } catch (Exception e) {
             return ResponseEntity
-            .status(HttpStatus.NOT_FOUND)
-            .body(e.toString());
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(e.toString());
         }
     }
-    
+
 }
