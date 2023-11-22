c032f885edd8d8e449718c154187eb04bb052b5b
VillalbaLucas <lukasvillalba2016@gmail.com>
2023-11-22 11:28:53 -0300

    add: modifyUser in UserController

--- src/main/java/com/SpringBanking/api/controllers/UserController.java
+++ src/main/java/com/SpringBanking/api/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -40,7 +41,9 @@ public class UserController {
         if (user.isPresent())
             return ResponseEntity.ok().body(user.get());
         else
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(user.get());
+            return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(user.get());
     }
 
     @PostMapping("/users")
@@ -62,7 +65,22 @@ public class UserController {
         if(userService.deleteById(id)){
             return ResponseEntity.ok().body(null);
         } else
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se pudo borrar el Usuario");
+            return ResponseEntity
+                .status(HttpStatus
+                .NOT_FOUND).body("No se pudo borrar el Usuario");
     }
 
+    @PatchMapping("/users/{id}")
+    public ResponseEntity<?> modifyUser(@PathVariable Long id, @RequestBody UserDto userDto){
+        UserDto userModify = userService.updateUser(id, userDto);
+        
+        if ( userModify != null) 
+            return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(userModify);
+        else
+            return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(userModify);
+    }
 }
