c88208aec8a27b0205735e493df497c1486b8702
VillalbaLucas <lukasvillalba2016@gmail.com>
2023-11-21 22:15:58 -0300

    create: repository for User

---
+++ src/main/java/com/SpringBanking/api/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.SpringBanking.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.SpringBanking.api.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+}
