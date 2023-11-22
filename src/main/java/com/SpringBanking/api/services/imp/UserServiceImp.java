d68d26f58ebce0058c514d8ef6b57c7cff5629d2
VillalbaLucas <lukasvillalba2016@gmail.com>
2023-11-22 11:27:02 -0300

    add: updateUser in UserServiceImp

--- src/main/java/com/SpringBanking/api/services/imp/UserServiceImp.java
+++ src/main/java/com/SpringBanking/api/services/imp/UserServiceImp.java
@@ -13,15 +13,15 @@ import com.SpringBanking.api.services.UserService;
 
 @Service
 public class UserServiceImp implements UserService {
-    
+
     private final UserRepository userRepo;
 
-    public UserServiceImp(UserRepository _userRepo){
-        userRepo=_userRepo;
+    public UserServiceImp(UserRepository _userRepo) {
+        userRepo = _userRepo;
     }
 
     @Override
-    public User save(UserDto userDto){
+    public User save(UserDto userDto) {
         User user = userRepo.save(UserMapper.dtoToUser(userDto));
         return user;
     }
@@ -33,17 +33,43 @@ public class UserServiceImp implements UserService {
 
     @Override
     public List<User> findAll() {
-       return userRepo.findAll();
+        return userRepo.findAll();
     }
 
     @Override
     public boolean deleteById(Long id) {
         Optional<User> user = userRepo.findById(id);
-        if(user.isPresent()){
+        if (user.isPresent()) {
             userRepo.deleteById(id);
             return true;
-        }
-        else
+        } else
             return false;
     }
+
+    @Override
+    public UserDto updateUser(Long id, UserDto userDto) {
+
+        if (userRepo.findById(id).isEmpty())
+            return null;
+        else {
+            User user = userRepo.findById(id).get();
+            
+            if (userDto.getUsername() != null)
+                user.setUsername(userDto.getUsername());
+            if (userDto.getEmail() != null)
+                user.setEmail(userDto.getEmail());
+            if (userDto.getPassword() != null)
+                user.setPassword(userDto.getPassword());
+            if (userDto.getDni() != null)
+                user.setDni(userDto.getDni());
+            if (userDto.getBirthdate() != null)
+                user.setBirthdate(userDto.getBirthdate());
+            if (userDto.getHomeaddres() != null)
+                user.setHomeaddres(userDto.getHomeaddres());
+
+            userRepo.save(user);
+            return UserMapper.userToDto(user);
+        }
+    }
+
 }
