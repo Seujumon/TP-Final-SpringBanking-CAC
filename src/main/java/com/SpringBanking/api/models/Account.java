02b1d9445ef1c7a21e2cbe1c33e4684c04b27f34
Seujumon <jseura@gmail.com>
2023-11-23 11:04:42 -0300

    Accounts entity created

---
+++ src/main/java/com/SpringBanking/api/models/Account.java
@@ -0,0 +1,34 @@
+package com.SpringBanking.api.models;
+
+import com.SpringBanking.api.models.enums.AccountType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "accounts")
+@Getter
+@Setter
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "account_type")
+    private AccountType type;
+    private String cbu;
+    private String alias;
+    private BigDecimal amount;
+    //TODO Relación con la clase USER
+    /*
+    @ManyToOne
+    private User owner;
+
+     */
+    //TODO Relación con la clase TRANSFER
+    /*
+    @OneToMany
+    private List<Transfer> transfers;
+    * */
+}
---
+++ src/main/java/com/SpringBanking/api/models/enums/AccountType.java
@@ -0,0 +1,5 @@
+package com.SpringBanking.api.models.enums;
+
+public enum AccountType {
+    SAVINGS_BANK, DOLLAR_SAVINGS_BANK, SAVING_ACCOUNT
+}
