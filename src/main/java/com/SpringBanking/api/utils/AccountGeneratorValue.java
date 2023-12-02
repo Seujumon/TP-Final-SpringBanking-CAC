0907a11580983a5285c6a0838109fe2fd024c70a
VillalbaLucas <lukasvillalba2016@gmail.com>
2023-12-01 22:37:14 -0300

    create: AccountGeneratorValue class for generate random cbu and default alias

---
+++ src/main/java/com/SpringBanking/api/utils/AccountGeneratorValue.java
@@ -0,0 +1,25 @@
+package com.SpringBanking.api.utils;
+
+import java.util.Random;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class AccountGeneratorValue {
+    private Random rand = new Random();
+    
+    public static String generateCbu(){
+        StringBuilder cbu = new StringBuilder();
+        for(int i=0; i<23; i++){
+            cbu.append(String.valueOf(rand.nextInt(10))); 
+        }
+        return String.valueOf(cbu);
+    }
+    public static String generateAlias(String name){
+        StringBuilder alias = new StringBuilder("Default." + name +".");
+        for (int i = 0; i < rand.nextInt(4, 8); i++) {
+            alias.append(rand.nextInt(10));
+        }
+        return String.valueOf(alias);
+    }
+}
