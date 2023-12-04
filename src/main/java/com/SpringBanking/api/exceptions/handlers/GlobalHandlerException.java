2dbda42240f995c6d55a5dce01d2a891c62ed7f3
VillalbaLucas <lukasvillalba2016@gmail.com>
2023-12-04 20:38:13 -0300

    add: handlerHttpMessageNotReadable and handlerNotFound methods

---
+++ src/main/java/com/SpringBanking/api/exceptions/handlers/GlobalHandlerException.java
@@ -0,0 +1,32 @@
+package com.SpringBanking.api.exceptions.handlers;
+
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.SpringBanking.api.exceptions.AccountNotExistsException;
+import com.SpringBanking.api.exceptions.TransferNotExistsException;
+import com.SpringBanking.api.exceptions.UserNotExistsException;
+
+@RestControllerAdvice
+public class GlobalHandlerException {
+    
+    @ExceptionHandler({UserNotExistsException.class, TransferNotExistsException.class, AccountNotExistsException.class})
+    public ResponseEntity<?> handlerClassNotExist(RuntimeException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<?> handlerNotFound(NotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Recurso no encontrado.\n" + ex.getMessage());
+    }
+
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> handlerHttpMessageNotReadable(HttpMessageNotReadableException ex){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Formato Json no Compatible. \n" + ex.getMessage());
+    }
+}
