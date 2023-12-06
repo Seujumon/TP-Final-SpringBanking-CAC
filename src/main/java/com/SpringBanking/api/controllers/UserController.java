f5a2174f583d9d59820ad792a1f3cbd043e22fe5
VillalbaLucas <lukasvillalba2016@gmail.com>
2023-12-05 23:21:00 -0300

    refact: delegate exceptions handling in UserController to GlobalHandlerException class
    Delego todo el manejo de las excepciones a un Handler que me maneja las excepciones globales de todos los controllers

--- src/main/java/com/SpringBanking/api/controllers/UserController.java
+++ src/main/java/com/SpringBanking/api/controllers/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.SpringBanking.api.exceptions.UserNotExistsException;
 import com.SpringBanking.api.models.dto.UserDto;
 import com.SpringBanking.api.services.UserService;
 
@@ -27,73 +26,38 @@ public class UserController {
 
     @GetMapping("/users")
     public ResponseEntity<?> getUsers() {
-        try {
             return ResponseEntity
                     .status(HttpStatus.OK)
                     .body(userService.findAll());
-            
-        } catch (Exception e) {
-            return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(e.getMessage());
-        }
     }
 
     @GetMapping("/users/{id}")
     public ResponseEntity<?> getUserById(@PathVariable Long id) {
-
-        try{
-            return ResponseEntity
-            .status(HttpStatus.OK)
-            .body(userService.findById(id));
-        }catch(UserNotExistsException e){
-            return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(e.getMessage());
-        }
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(userService.findById(id));
     }
 
     @PostMapping("/users")
     public ResponseEntity<?> saveUser(@RequestBody UserDto userDto) {
-        try {
             return ResponseEntity
                     .ok()
                     .body(userService.save(userDto));
-        } catch (Exception e) {
-            return ResponseEntity
-                    .status(HttpStatus.NOT_FOUND)
-                    .body(e.getMessage());
-        }
     }
 
     @DeleteMapping("/users/{id}")
-    public ResponseEntity<?> deleteUserById(@PathVariable Long id){
-
-        try{
+    public ResponseEntity<?> deleteUserById(@PathVariable Long id) {
             userService.deleteById(id);
-
             return ResponseEntity
-            .status(HttpStatus.OK)
-            .body("Cuenta con id: "+id+" ha sido eliminada!");
-        } catch(UserNotExistsException e){
-            return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(e.getMessage());
-        }
+                    .status(HttpStatus.OK)
+                    .body("Cuenta con id: " + id + " ha sido eliminada!");
     }
 
     @PatchMapping("/users/{id}")
-    public ResponseEntity<?> modifyUser(@PathVariable Long id, @RequestBody UserDto userDto){
+    public ResponseEntity<?> modifyUser(@PathVariable Long id, @RequestBody UserDto userDto) {
         UserDto userModify = userService.updateUser(id, userDto);
-        
-        try{
-            return ResponseEntity
+        return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(userModify);
-        } catch(Exception e){
-            return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(e.getMessage());
-        }
     }
 }
