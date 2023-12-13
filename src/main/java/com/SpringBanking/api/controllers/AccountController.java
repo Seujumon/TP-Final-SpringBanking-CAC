6c5a32f0322b9bcbbc8201b9e28a1db8cef8343f
VillalbaLucas <lukasvillalba2016@gmail.com>
2023-12-13 11:27:46 -0300

    refact: AccountController

--- src/main/java/com/SpringBanking/api/controllers/AccountController.java
+++ src/main/java/com/SpringBanking/api/controllers/AccountController.java
@@ -1,14 +1,10 @@
 package com.SpringBanking.api.controllers;
 
-import com.SpringBanking.api.exceptions.AccountNotExistsException;
 import com.SpringBanking.api.models.dto.AccountDto;
 import com.SpringBanking.api.services.AccountService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.List;
 
@@ -22,55 +18,29 @@ public class AccountController {
     this.service = service;
   }
 
-
-
   @GetMapping(value = "/{id}")
   public ResponseEntity<?> getAccount(@PathVariable Long id) {
-    try {
-      return ResponseEntity.status(HttpStatus.OK).body(service.getAccountById(id));
-    } catch (AccountNotExistsException e) {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-    }
+    return ResponseEntity.status(HttpStatus.OK).body(service.getAccountById(id));
   }
 
-   @GetMapping
-    public ResponseEntity<List<AccountDto>> getAccounts() {
-        try {
-            List<AccountDto> lista = service.getAccounts();
-            return ResponseEntity.status(HttpStatus.OK).body(lista);
-        } catch (Exception ex) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-    }
-  
+  @GetMapping
+  public ResponseEntity<List<AccountDto>> getAccounts() {
+    return ResponseEntity.status(HttpStatus.OK).body(service.getAccounts());
+  }
 
-@PutMapping(value = "/{id}")
-public ResponseEntity<AccountDto> updateAccount(@PathVariable Long id, @RequestBody AccountDto dto) {
-    try {
+  @PutMapping(value = "/{id}")
+  public ResponseEntity<AccountDto> updateAccount(@PathVariable Long id, @RequestBody AccountDto dto) {
     return ResponseEntity.status(HttpStatus.OK).body(service.updateAccount(id, dto));
-  } catch (Exception e) {
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(service.updateAccount(id, dto));
   }
-}
-  
+
   @PostMapping("/{idUser}")
-  public ResponseEntity<?> createAccount(@PathVariable Long idUser, @RequestBody AccountDto dto){
-      try {
-          return ResponseEntity.status(HttpStatus.CREATED).body(service.createAccount(idUser, dto));
-      } catch (Exception e) {
-          return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-      }
+  public ResponseEntity<?> createAccount(@PathVariable Long idUser, @RequestBody AccountDto dto) throws Exception {
+    return ResponseEntity.status(HttpStatus.CREATED).body(service.createAccount(idUser, dto));
   }
 
-  
-
-    @DeleteMapping(value = "/{id}")
-    public ResponseEntity<String> deleteAccount(@PathVariable Long id) {
-        try {
-            String deletionResult = service.deleteAccount(id);
-            return ResponseEntity.status(HttpStatus.OK).body(deletionResult);
-        } catch (Exception ex) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-    }
+  @DeleteMapping(value = "/{id}")
+  public ResponseEntity<String> deleteAccount(@PathVariable Long id) {
+      String deletionResult = service.deleteAccount(id);
+      return ResponseEntity.status(HttpStatus.OK).body(deletionResult);
+  }
 }
